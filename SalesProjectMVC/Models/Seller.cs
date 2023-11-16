0fb6d889e3567810cdd0eaffcdbcdf94a01193c9
Jeferson Heidrich <jheidrich2@gmail.com>
2023-11-16 17:37:20 -0300

    Formatting dates, table labels and values

--- SalesProjectMVC/Models/Seller.cs
+++ SalesProjectMVC/Models/Seller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq; 
+using System.Linq;
+using System.ComponentModel.DataAnnotations; 
 
 namespace SalesProjectMVC.Models
 {
@@ -8,9 +9,16 @@ namespace SalesProjectMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string EMail { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+        [Display(Name = "Department Name")]
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); 
--- SalesProjectMVC/Startup.cs
+++ SalesProjectMVC/Startup.cs
@@ -9,7 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization; 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization; 
 using SalesProjectMVC.Data;
 using SalesProjectMVC.Services;
 
@@ -48,6 +50,16 @@ namespace SalesProjectMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizatonOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizatonOptions); 
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
